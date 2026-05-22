cc6572fa561f63b0c46c94878a19fba7df7e3f19
Lah-deh <successasokere@gmail.com>
2026-05-22 13:28:01 +0100

    chore: updated the gitignore file

---
+++ secrets.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC6OE3-jl73z41B4rWEO699fBBaqJPQPRI",
+  authDomain: "water-tank-monitoring-sy-9fbf3.firebaseapp.com",
+  databaseURL: "https://water-tank-monitoring-sy-9fbf3-default-rtdb.firebaseio.com",
+  projectId: "water-tank-monitoring-sy-9fbf3",
+  storageBucket: "water-tank-monitoring-sy-9fbf3.firebasestorage.app",
+  messagingSenderId: "250565556537",
+  appId: "1:250565556537:web:428aec758e7c0155b7d0e8",
+  measurementId: "G-XQVNN1GPRM"
+};
